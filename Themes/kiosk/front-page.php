f427933a3f8523409975a8ffcf16d0d8ce62e652
azizulsoftsw <azizul.softsw@gmail.com>
2020-02-15 00:11:48 +0600

    page

--- Themes/kiosk/front-page.php
+++ Themes/kiosk/front-page.php
@@ -2,7 +2,14 @@
 <?php
   $intro = get_field('hintro', HOMEID);
   $hbox1 = get_field('hbox1', HOMEID);
+  $box1link = $box2link = 'javascript:void();';
+  if( !empty($hbox1['link']) ){
+    $box1link = $hbox1['link'];
+  }
   $hbox2 = get_field('hbox2', HOMEID);
+  if( !empty($hbox2['link']) ){
+    $box2link = $hbox2['link'];
+  }
 ?>
 <section class="hm-content-sec-wrp">
   <div class="container-xlg">
@@ -14,14 +21,14 @@
               <div class="hm-dsc-box-lft clearfix">
                 <div class="hm-dsc-box-bg">
                   <div class="hm-dsc-box">
-                    <?php if( !empty( $hbox1['title'] ) ) printf( '<h1>%s</h1>', $hbox1['title']); ?>
+                    <?php if( !empty( $hbox1['title'] ) ) printf( '<h1><a href="%s">%s</a></h1>', $box1link, $hbox1['title']); ?>
                   </div>
                 </div>
               </div>
               <div class="hm-dsc-box-rgt clearfix">
                 <div class="hm-dsc-box-bg">
                   <div class="hm-dsc-box">
-                    <?php if( !empty( $hbox2['title'] ) ) printf( '<h1>%s</h1>', $hbox2['title']); ?>
+                    <?php if( !empty( $hbox2['title'] ) ) printf( '<h1><a href="%s">%s</a></h1>', $box2link, $hbox2['title']); ?>
                   </div>
                 </div>
               </div>
--- Themes/kiosk/page.php
+++ Themes/kiosk/page.php
@@ -1,56 +1,16 @@
-<?php get_header(); ?>
+<?php 
+get_header(); 
+while( have_posts() ): the_post();
+?>
 <section class="ks-dsc-sec-wrp">
   <div class="container-xlg">
     <div class="row">
       <div class="col-sm-12">
         <div class="ks-dsc-wrp">
-          <span>Pagebuilder field</span>
-          <h1>H1</h1>
-          <p>Paragraph <strong>Bold</strong> <a href="#">Link</a></p>
-          <em>Italic</em>
-          <h2>H2</h2>
-          <p>Paragraph <strong>Bold</strong> <a href="#">Link</a></p>
-          <em>Italic</em>
+          <?php the_content(); ?>
         </div>
       </div>
     </div>
   </div>
 </section>
-
-<section class="ks-form-sec-wrp">
-  <div class="container-xlg">
-    <div class="row">
-      <div class="col-sm-12">
-        <div class="ks-form-block-wrp">
-          <div id="ks-form-wrp">
-            <span>Form Layout:</span>
-            <div class="wpforms-container">
-              <form class="wpforms-form">
-                <div class="wpforms-field-container">
-                  <div class="wpforms-field">
-                    <input type="text" name="name" placeholder="[ Placeholder ]" required>
-                  </div>
-                  
-                  <div class="wpforms-field">
-                    <input type="email" name="email" placeholder="[ Placeholder ]" required>
-                  </div>
-
-                  <div class="wpforms-field yourFieldFull">
-                    <input type="text" name="email" placeholder="[ Placeholder ]" required>
-                  </div>
-
-                </div><!-- end of .wpforms-field-container-->
-
-                <div class="wpforms-submit-container">
-                  <button type="submit" name="submit" class="wpforms-submit">Send button style</button>
-                </div>
-
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<?php get_footer(); ?>
+<?php endwhile; get_footer(); ?>
